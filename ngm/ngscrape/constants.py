174088b33191e0062b9385603571bc3218de2ee1
Damodar Dahal <damo94761@gmail.com>
2026-01-27 08:50:55 -0800

    scrape 10 years cases

--- ngm/ngscrape/constants.py
+++ ngm/ngscrape/constants.py
@@ -1,7 +1,7 @@
 """Constants for court case scrapers."""
 
 # Scraping configuration
-SCRAPE_LOOKBACK_DAYS = 5 * 365  # How many days back to scrape
+SCRAPE_LOOKBACK_DAYS = 10 * 365  # How many days back to scrape
 SCRAPE_OFFSET_DAYS = 2  # Days to offset from today (to avoid incomplete data)
 
 SCRAPE_LOOKBACK_DAYS_SUPREME_COURT = 15 * 365
